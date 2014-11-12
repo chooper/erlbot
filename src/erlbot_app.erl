e8f7996b1fe39aba56006a6807ee9e0f667cbdc3
Charles Hooper <chooper@plumata.com>
2014-11-11 22:14:04 -0800

    use lager and auto-op everyone

--- src/erlbot_app.erl
+++ src/erlbot_app.erl
@@ -6,6 +6,7 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
+    lager:start(),
     erlbot_sup:start_link().
 
 stop(_State) ->
--- src/erlbot_callbacks.erl
+++ src/erlbot_callbacks.erl
@@ -1,4 +1,5 @@
 -module(erlbot_callbacks).
+-compile([{parse_transform, lager_transform}]).
 
 -behavior(gen_server).
 
@@ -20,24 +21,33 @@ start_link() ->
 
 init([]) ->
     irc_lib_sup:start_link(),
-    %% dev.pearachute.net = 72.14.176.182
-    irc_lib_sup:start_irc_client(?MODULE, {<<"dev.pearachute.net">>, <<>>}, 6667, [{<<"#hello">>, <<>>}], <<"mynameiserl">>, false, 30000),
+    {ok, ClientPid} = irc_lib_sup:start_irc_client(?MODULE, {<<"dev.pearachute.net">>, <<>>}, 6667, [{<<"#bottest">>, <<>>}], <<"mynameiserl">>, false, 30000),
+    register(irc_client_pid, ClientPid),
+    {ok, _} = timer:send_interval(timer:seconds(10), ClientPid, {raw, "NAMES #bottest"}), %% TODO don't hardcode this
     {ok, #state{}}.
  
 handle_call(Request, From, State) ->
-    io:format("handle_call: ~p ~p ~p ~n", [Request, From, State]),
+    lager:debug("handle_call: ~p ~p ~p ~n", [Request, From, State]),
     {reply, ignored, State}.
 
 handle_cast(Msg, State) ->
-    io:format("handle_cast: ~p ~p ~n", [Msg, State]),
+    lager:debug("handle_cast: ~p ~p ~n", [Msg, State]),
     {noreply, State}.
 
 handle_info({incoming_message, From, IncomingMessage}, State) ->
-    io:format("Incoming message: ~p ~p ~n", [IncomingMessage, From]),
+    lager:debug("Incoming message: ~p ~p ~n", [IncomingMessage, From]),
+    {noreply, State};
+
+handle_info({names, Channel, Names}, State) ->
+    lists:foreach(fun(Name) ->
+                    timer:sleep(200),
+                    irc_client_pid ! {raw, "MODE " ++ Channel ++ " +o " ++ Name}
+                  end,
+                  Names),
     {noreply, State};
 
 handle_info(Info, State) ->
-    io:format("handle_info: ~p ~p ~n", [Info, State]),
+    lager:debug("handle_info: ~p ~p ~n", [Info, State]),
     {noreply, State}.
 
 terminate(_Reason, _State) ->
