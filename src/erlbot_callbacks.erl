949dfd455b08e2826cccc3e3275b9a7bf95a2858
Charles Hooper <chooper@plumata.com>
2014-11-11 16:49:01 -0800

    debug prints :S

--- src/erlbot_callbacks.erl
+++ src/erlbot_callbacks.erl
@@ -24,17 +24,20 @@ init([]) ->
     irc_lib_sup:start_irc_client(?MODULE, {<<"dev.pearachute.net">>, <<>>}, 6667, [{<<"#hello">>, <<>>}], <<"mynameiserl">>, false, 30000),
     {ok, #state{}}.
  
-handle_call(_Request, _From, State) ->
+handle_call(Request, From, State) ->
+    io:format("handle_call: ~p ~p ~p ~n", [Request, From, State]),
     {reply, ignored, State}.
 
-handle_cast(_Msg, State) ->
+handle_cast(Msg, State) ->
+    io:format("handle_cast: ~p ~p ~n", [Msg, State]),
     {noreply, State}.
 
 handle_info({incoming_message, From, IncomingMessage}, State) ->
     io:format("Incoming message: ~p ~p ~n", [IncomingMessage, From]),
     {noreply, State};
 
-handle_info(_Info, State) ->
+handle_info(Info, State) ->
+    io:format("handle_info: ~p ~p ~n", [Info, State]),
     {noreply, State}.
 
 terminate(_Reason, _State) ->
